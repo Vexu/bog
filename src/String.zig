b173879ebb49d407f624ad7beeac22f478e69535
Vexu <git@vexu.eu>
2020-12-10 12:11:32 +0200

    support basic string formatting

--- src/String.zig
+++ src/String.zig
@@ -24,6 +24,11 @@ pub const Builder = struct {
         };
     }
 
+    pub fn cancel(b: *Builder) void {
+        b.inner.deinit();
+        b.* = undefined;
+    }
+
     pub fn append(b: *Builder, data: []const u8) !void {
         try b.inner.appendSlice(data);
     }
@@ -47,20 +52,6 @@ pub fn builder(allocator: *Allocator) Builder {
     return Builder{
         .inner = std.ArrayList(u8).init(allocator),
     };
-}
-
-pub fn toBuilder(string: String, allocator: *Allocator) !Builder {
-    if (string.capacity == 0) {
-        var b = Builder{
-            .inner = std.ArrayList(u8).init(allocator),
-        };
-        try b.append(string.data);
-        return b;
-    } else {
-        return Builder{
-            .inner = std.ArrayList(u8).fromOwnedSlice(allocator, @bitCast([]u8, string.data)),
-        };
-    }
 }
 
 pub fn deinit(str: *String, allocator: *Allocator) void {
@@ -178,19 +169,83 @@ pub fn in(str: *const String, val: *const Value) bool {
 }
 
 pub fn append(str: *String, allocator: *Allocator, data: []const u8) !void {
-    var b = try str.toBuilder(allocator);
+    var b = builder(allocator);
+    errdefer b.cancel();
+
+    try b.append(str.data);
     try b.append(data);
+
+    str.deinit(allocator);
     str.* = b.finish();
 }
 
 pub fn format(str: String, vm: *Vm, args: []const *Value) !*Value {
     var b = builder(vm.gc.gpa);
-    // TODO proper formatting
-    try b.append(str.data);
-    for (args) |arg| {
-        try arg.dump(b.writer(), default_dump_depth);
+    errdefer b.cancel();
+    try b.inner.ensureCapacity(str.data.len);
+
+    const w = b.writer();
+    var state: enum {
+        start,
+        brace,
+        format,
+    } = .start;
+    var arg_i: usize = 0;
+    var format_start: usize = 0;
+    var options = std.fmt.FormatOptions{};
+
+    for (str.data) |c, i| {
+        switch (state) {
+            .start => if (c == '{') {
+                state = .brace;
+            } else {
+                try w.writeByte(c);
+            },
+            .brace => if (c == '{') {
+                try w.writeByte(c);
+            } else {
+                if (arg_i >= args.len) {
+                    return vm.reportErr("too few arguments");
+                }
+                format_start = i;
+                state = .format;
+                options = .{};
+            },
+            .format => if (c == '}') {
+                const fmt = str.data[format_start..i];
+                var fmt_type: u8 = 0;
+
+                if (fmt.len == 1) {
+                    fmt_type = fmt[0];
+                } else {
+                    // TODO properly parse format options
+                }
+
+                switch (fmt_type) {
+                    'x', 'X' => {
+                        if (args[arg_i].* != .int) {
+                            return vm.reportErr("expected int");
+                        }
+                        try std.fmt.formatInt(args[arg_i].int, 16, fmt[0] == 'X', options, w);
+                    },
+                    0 => {
+                        try args[arg_i].dump(w, default_dump_depth);
+                    },
+                    else => {
+                        return vm.reportErr("unknown format specifier");
+                    },
+                }
+
+                state = .start;
+                arg_i += 1;
+            },
+        }
     }
+    if (arg_i != args.len) {
+        return vm.reportErr("unused arguments");
+    }
+
     const ret = try vm.gc.alloc();
-    ret.* = Value{ .str = b.finish()};
+    ret.* = Value{ .str = b.finish() };
     return ret;
 }
