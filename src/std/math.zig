59680aab0a69261be8de70d8c4d8afbd12665a5d
data-man <datamanrb@gmail.com>
2021-01-29 09:47:05 +0000

    Add bitops to std

--- src/std/math.zig
+++ src/std/math.zig
@@ -219,3 +219,15 @@ pub fn sin(val: f64) f64 {
 pub fn tan(val: f64) f64 {
     return math.tan(val);
 }
+
+pub fn clz(val: i64) i64 {
+    return @clz(i64, val);
+}
+
+pub fn ctz(val: i64) i64 {
+    return @ctz(i64, val);
+}
+
+pub fn popcount(val: i64) i64 {
+    return @popCount(i64, val);
+}
